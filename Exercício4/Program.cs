1739c29775b1f9daa359292c711dbc3e0e887dda
Jayme <jaymequimica@hotmail.com>
2022-02-22 15:24:49 -0300

    mais exercícios de dotnet

---
+++ Exercício4/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+Pessoa p1 = new Pessoa();
+
+p1.Nome = "Ricardo";
+p1.Idade = 30;
+p1.EnderecoPessoa = new Endereco()
+{
+    Logradouro = "Rua teste",
+    CEP = "00000",
+    Cidade = "São Paulo",
+    Numero = 300
+};
+
+WriteLine("Fim");
